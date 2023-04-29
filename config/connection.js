8c1b8d50c23354c34d89af178bd4a237175f02ed
Laurie Fish <iamlauriefish@gmail.com>
2023-04-29 13:41:40 -0700

    added tables

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const connection = mysql.createConnection(
     {
-        host: 'localhost'
+        host: 'localhost',
         port: 3306,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
     });
-
+connection.connect();
 module.exports = connection;
---
+++ server.js
@@ -0,0 +1,4 @@
+const inquirer = require('inquirer');
+const connection = require('./config/connection');
+const db = connection;
+const mysql = require('mysql2');
